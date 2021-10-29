f682eb1e3ee89a1f131d5d44244a219242f140e6
Utkarsh Agnihotri <56087687+utkhagni13@users.noreply.github.com>
2021-10-29 12:24:03 +0530

    corrected error messages

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -4,14 +4,15 @@ const config = require("../config/Keys");
 
 module.exports.getSessionToken = async (req, res) => {
     const token = req.body.jwttoken;
-
     if (!token) {
-        return res.status(400).json({ response: null, error: "Token expired" });
+        return res.status(400).json({ response: null, error: "Token empty" });
     }
     try {
+        console.log(config.jwtSecret);
         const decoded = await jwt.verify(token, config.jwtSecret);
+        console.log(decoded);
         return res.status(200).json({ response: decoded, error: null });
     } catch (err) {
-        return res.status(401).json({ response: null, error: "Invalid token" });
+        return res.status(401).json({ response: null, error: "Token expired" });
     }
 };
